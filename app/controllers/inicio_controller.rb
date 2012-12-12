0206b82ed53daacfd11881c60a85cc6fb71b4275
lcssimonini <lcs.simonini@gmail.com>
2012-12-11 23:12:13 -0200

    tabela de entrada de targets

--- app/controllers/inicio_controller.rb
+++ app/controllers/inicio_controller.rb
@@ -7,11 +7,25 @@ class InicioController < ApplicationController
 
 	def salva_tabela
 		@tabela = TabelaVerdade.new(vars: params[:vars], nome: params[:nome])
+		@tabela.linhas = cria_linhas_tabela @tabela.vars
 		@tabela.save
 		redirect_to :back
 	end
 
 	def show_tabela
 		@tabela = TabelaVerdade.find params[:id]
+	end
+
+	def cria_linhas_tabela vars
+		nl = 2**vars
+		tabela = Array.new
+		for num in 0..(nl-1) do
+		   linha = Array.new
+		   string = sprintf "%0#{vars}b", num
+		   string.each_char do |c| linha << c.to_i end
+		   linha = linha.map{|x| x == 0 ? x = -1 : x}
+		   tabela << linha
+		end
+		return tabela
 	end
 end
